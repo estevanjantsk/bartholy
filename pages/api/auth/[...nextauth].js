571a7851b4e9d38994c2c9edac77f99a84effa64
Estevan Jantsk <estevan.jantsk@pipedrive.com>
2022-05-06 17:13:15 +0300

    add base nextauth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,7 @@
+import NextAuth from "next-auth"
+import SlackProvider from "next-auth/providers/slack"
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [],
+})
